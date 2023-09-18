fbdad9dd4a24a0ef31fb79b47b5ee631196e4fe4
vagrant <vagrant@node3>
2023-09-18 07:17:29 +0000

    ok

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ cd "$(dirname "$0")"
 mkdir -p ~/.vim/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp ./.vimrc ~/.vimrc
 vim +'PlugInstall --sync' +qa
 
-cp ./.vimrc ~/.vimrc
 
 echo "\nDone"
 
